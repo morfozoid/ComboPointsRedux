ac341c64c8464bc7408f47789eae6e23566fdce2
Michael Murray <mjmurray88@gmail.com>
2016-07-29 15:20:04 -0500

    Name the global Graphics and Text frames

--- Core.lua
+++ Core.lua
@@ -501,7 +501,8 @@ end
 
 local bg = {bgFile = "Interface\\Tooltips\\UI-Tooltip-Background"}
 function ComboPointsRedux:MakeTextFrame(moduleName)
-	local f = CreateFrame("FRAME", nil, UIParent)
+	local frameName = string.upper(moduleName:gsub("%s", ""))
+	local f = CreateFrame("FRAME", "CPR_"..frameName.."TEXT", UIParent)
 	f.moduleName = moduleName
 	f.moduleType = "text"
 	
@@ -562,7 +563,8 @@ function ComboPointsRedux:MakeTextFrame(moduleName)
 end
 
 function ComboPointsRedux:MakeGraphicsFrame(moduleName, num)
-	local g = CreateFrame("FRAME", nil, UIParent)
+	local frameName = string.upper(moduleName:gsub("%s", ""))
+	local g = CreateFrame("FRAME", "CPR_"..frameName.."GRAPHICS", UIParent)
 	g.moduleName = moduleName
 	g.moduleType = "graphics"
 	
