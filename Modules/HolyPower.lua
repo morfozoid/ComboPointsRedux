b677a381cf2e4ae9c753cc70907568580befd005
morfozoid <morfhero@gmail.com>
2023-11-12 16:19:09 +0300

    Update HolyPower.lua
    undo evil copy-paste

--- Modules/HolyPower.lua
+++ Modules/HolyPower.lua
@@ -1,36 +1,36 @@
 --[[
 Author: Starinnia
 CPR is a combo points display addon based on Funkydude's BasicComboPoints
-SoulShards.lua - A module for tracking Soul Shards
-Last File Hash: 7cf8e1e
-Last File Date: 2023-11-07T7:19:35Z
-Project Version: 5.0.3.2
+HolyPower.lua - A module for tracking Holy Power
+Last File Hash: @file-abbreviated-hash@
+Last File Date: @file-date-iso@
+Project Version: @project-version@
 contact: codemaster2010 AT gmail DOT com
 
 Copyright (c) 2007-2017 Michael J. Murray aka Lyte of Lothar(US)
 All rights reserved unless otherwise explicitly stated.
 ]]
 
-if select(2, UnitClass("player")) ~= "WARLOCK" then return end
+if select(2, UnitClass("player")) ~= "PALADIN" then return end
 
 local UnitPower = UnitPower
-local SPELL_POWER_SOUL_SHARDS = Enum.PowerType.SoulShards
+local SPELL_POWER_HOLY_POWER = Enum.PowerType.HolyPower
 
 local cpr = LibStub("AceAddon-3.0"):GetAddon("ComboPointsRedux")
-local modName = "Soul Shards"
+local modName = "Holy Power"
 local mod = cpr:NewModule(modName)
 
 function mod:OnInitialize()
-	self.MAX_POINTS = UnitPowerMax("player",SPELL_POWER_SOUL_SHARDS)
-	self.Count = UnitPower("player", SPELL_POWER_SOUL_SHARDS)
-	self.displayName = SOUL_SHARDS_POWER
-	self.abbrev = "SS"
+	self.MAX_POINTS = UnitPowerMax("player",SPELL_POWER_HOLY_POWER)
+	self.Count = UnitPower("player", SPELL_POWER_HOLY_POWER)
+	self.displayName = HOLY_POWER
+	self.abbrev = "HP"
 	self.events = { ["UNIT_POWER_UPDATE"] = "Update", ["SPELLS_CHANGED"] = "UpdateMaxPoints", ["UNIT_DISPLAYPOWER"] = "Update", ["PLAYER_SPECIALIZATION_CHANGED"] = "UpdateMaxPoints", ["PLAYER_LOGIN"] = "UpdateMaxPoints", ["PLAYER_ENTERING_WORLD"] = "UpdateMaxPoints" }
 end
 
 local oldCount = 0
 function mod:Update()
-	self.Count = UnitPower("player", SPELL_POWER_SOUL_SHARDS) or 0
+	self.Count = UnitPower("player", SPELL_POWER_HOLY_POWER) or 0
 	local r, g, b = cpr:GetColorByPoints(modName, self.Count)
 	local a, a2 = cpr:GetAlphas(modName)
 	
@@ -70,10 +70,10 @@ function mod:Update()
 end
 
 function mod:UpdateMaxPoints()
-	self.Count = UnitPower("player", SPELL_POWER_SOUL_SHARDS)
+	self.Count = UnitPower("player", SPELL_POWER_HOLY_POWER)
 	local a, a2 = cpr:GetAlphas(modName)
 	
-	self.MAX_POINTS = UnitPowerMax("player",SPELL_POWER_SOUL_SHARDS)
+	self.MAX_POINTS = UnitPowerMax("player",Enum.PowerType.HolyPower)
 	if self.graphics then
 		for i = 1, 8 do
 			self.graphics.points[i]:Hide()
@@ -91,4 +91,4 @@ function mod:UpdateMaxPoints()
 		end
 	end
 	cpr:UpdateSettings(modName)
-end							  
+end
